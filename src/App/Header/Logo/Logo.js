860df08915eba9b3645768f852974fbd655eecbb
Yuliia <yuliia.brakova@gmail.com>
2020-03-24 16:37:09 +0200

    доюавила link для  menu/logo

--- src/App/Header/Logo/Logo.js
+++ src/App/Header/Logo/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 import logo from './logo.png'
 
 const Logo = () => {
     return (
         <div className="logo_hotel">
-            <a> <img src={logo} alt="logo"/></a>
+            <Link to='/'> <img src={logo} alt="logo"/></Link>
         </div>
     )
 }
--- src/App/Header/Menu/Menu.js
+++ src/App/Header/Menu/Menu.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 
+import './menu.css'
 
 const Menu = () => {
     return (
         <div className="nav">
-            <a href="" className="nav_link">HOME</a>
-            <a href="" className="nav_link">ABOUT US</a>
-            <a href="" className="nav_link">ROOMS</a>
-            <a href="" className="nav_link">PORTFOLIO</a>
-            <a href="" className="nav_link">BLOG</a>
-            <a href="" className="nav_link">CONTACT</a>
+             <Link to='/' className="nav_link">HOME</Link>
+             <Link to='/' className="nav_link">ABOUT US</Link>
+             <Link to='/' className="nav_link">ROOMS</Link>
+             <Link to='/' className="nav_link">PORTFOLIO</Link>
+             <Link to='/' className="nav_link">BLOG</Link>
+             <Link to='/' className="nav_link">CONTACT</Link>
         </div>
     )
 }
