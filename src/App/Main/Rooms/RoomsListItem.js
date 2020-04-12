cfcc39dd37309b2a2c682817f3e0d42c9077837a
Yuliia <yuliia.brakova@gmail.com>
2020-04-12 18:47:54 +0300

    добавила like

--- src/App/Main/Rooms/RoomsListItem.js
+++ src/App/Main/Rooms/RoomsListItem.js
@@ -3,21 +3,36 @@ import PropTypes from 'prop-types'
 
 
 class RoomsListItem extends Component{
+    state = {
+        liked: false
+      };
+
+      handleClick= () => {
+        this.setState({
+          liked: !this.state.liked
+        });
+      }
+
+  
 
     render (){
+        const label = this.state.liked ? <span>&#9829;</span> : <span>&#9825;</span>
         const{
             
             name,
             description,
 
             image,
-          
+           
         } = this.props;
 
         return (
             <div className="hotel_list">
                     <div className="hotel_list_img">
                          <img src={image} alt={description} title={name} />
+                          <button class="heart" onClick={this.handleClick} >
+                          {label}
+                          </button>
                     </div>
                 <div className="hotel_list_description">
                     <a href=""><h2>{name}</h2></a>
